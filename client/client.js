84ee0ef5cd76c97ca55e02885eacede8153b7845
lpeng32 <mintylilac@yahoo.com>
2014-03-06 15:35:39 -0500

    start room implementation
    updated the main chatroom. added a button that will lead players to
    rooms to play the game. also, LOOK AT THE TODO

--- client/client.js
+++ client/client.js
@@ -2,7 +2,19 @@
 * Templates
 */
 Template.messages.messages = function () {
-  return Messages.find({}, { sort: { time: -1 }});
+  return Messages.find({}, { sort: { time: +1 }});
+  
+  //return messages.find({room:Session.get("room")}) //get the room name set in the router
+}
+
+Template.lobby.events = {
+  'click input' : function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      //here is what happens if you click the button
+      
+      console.log("You pressed the button");
+    }
 }
 
 Template.input.events = {
@@ -11,7 +23,8 @@ Template.input.events = {
       if (Meteor.user())
         var name = Meteor.user().profile.name;
       else
-        var name = 'Anonymous';
+        //here you can try to do Guest1, Guest2, etc.
+        var name = 'Guest';
       var message = document.getElementById('message');
 
       if (message.value != '') {
@@ -27,5 +40,14 @@ Template.input.events = {
     }
   }
 }
+
+/*Meteor.Router.add({
+  '/': 'home',
+  '/rooms/:id': function(id) {
+     Session.set("room",id); //set the room for the template
+     return "messages"; //If you're template is called messages
+  },
+  '*': 'not_found'
+});*/
 
 
