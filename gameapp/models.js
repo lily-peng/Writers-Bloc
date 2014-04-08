6c68861ff2a8d95e8eb61bde731886406c6d09b3
simon <simon@simon-laptop.(none)>
2014-04-08 10:44:51 -0400

    CollectionTest contains basic client/server interaction code for reference

--- gameapp/models.js
+++
@@ -1,2 +0,0 @@
-Games = new Meteor.Collection('games'); // {gameId, numPlayers, stage}
-Sentences = new Meteor.Collection('sentences'); // {gameId, playerId, roundNumber, text, numVotes, randomNumber}
