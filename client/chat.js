b1109122cf4f72b725a555c2054e4fb7b316663c
lpeng32 <mintylilac@yahoo.com>
2014-04-21 01:31:01 -0400

    html/css formatting to look nice
    LP

--- client/chat.js
+++ client/chat.js
@@ -1,20 +1,29 @@
-Template.chat.helpers({
-  messages: function() {
-    var messages = Messages.find({}, {sort: {Date: 1}});
-    return messages;
+  Template.chat.helpers({
+    messages: function() {
+      var messages = Messages.find({}, {sort: {Date: 1}});
+      return messages;
+    }
+  });
+  
+  Template.chat.events({
+    'click #submitMessage': function(event) {
+      var gameID = Session.get("gameID");
+      var playerID = Session.get("playerID");
+      var playerName = Players.findOne(playerID).Name;
+      var textbox = document.getElementById("chatText");
+      var text = textbox.value;
+      textbox.value = "";
+      Meteor.call("insertMessage", gameID, playerName, text);
+      var history = document.getElementById("chatHistory");
+      history.scrollTop = history.scrollHeight;
+    }
+  });
+  
+  scrolling = function() {
+      Meteor.setInterval(scrollToBottom, 100);
   }
-});
-
-Template.chat.events({
-  'click #submitMessage': function(event) {
-    var gameID = Session.get("gameID");
-    var playerID = Session.get("playerID");
-    var playerName = Players.findOne(playerID).Name;
-    var textbox = document.getElementById("chatText");
-    var text = textbox.value;
-    textbox.value = "";
-    Meteor.call("insertMessage", gameID, playerName, text);
-    var history = document.getElementById("chatHistory");
-    history.scrollTop = history.scrollHeight;
+  
+  scrollToBottom = function() {
+      var objDiv = document.getElementById("chatHistory");
+      objDiv.scrollTop = objDiv.scrollHeight;
   }
-});
